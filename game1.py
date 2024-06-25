e959fd548c91b06cad15e2450df095d9d59665be
GaetanL <49320055+almatsy159@users.noreply.github.com>
2024-06-25 02:42:00 +0200

    Update game1.py
    merge game and game1

--- game1.py
+++ game1.py
@@ -15,5 +15,16 @@ class SpellMaker:
         
         # select targets
         #
-        # 
+
+"""
+un  joueur lance une boule de feu sur une porte :
+
+    il materialise la boule de feu = > transfert de mana 
+
+"""
+
+class Spell :
+    def __init__(self,object=None,):
+        pass
+    
         
